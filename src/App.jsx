369a12f61b9a74f12216668a8ce2d9ba9e0ef27f
JasonXenon <levecqjason@gmail.com>
2024-12-09 23:49:41 +0100

    implement local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import Form from "./components/Form";
 import List from "./components/List";
 
+
 function App() {
     // État pour stocker les tâches
-    const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState(() => {
+        const savedTasks = localStorage.getItem("tasks");   // Récupère les tâches sauvegardées dans le localStorage
+        return savedTasks ? JSON.parse(savedTasks) : [];    // Si des tâches sont sauvegardées, les retourne, sinon retourne un tableau vide
+    });
+
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(tasks));   // Sauvegarde les tâches dans le localStorage
+    }, [tasks]);
 
     // Fonction pour ajouter une tâche
     const addTask = (task) => {
